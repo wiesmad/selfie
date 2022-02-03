1db11154bd0f6fa3638bbf951164287a01ab37f7
Wiesław Mazurak <wmazurak@gamil.com>
2022-02-03 15:36:44 +0100

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { json } = require('express/lib/response');
+const Datastore = require('nedb');
+const app = express();
+
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+const  database = new Datastore('database.db');
+database.loadDatabase();
+
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+        if (err) {
+            response.end();
+            console.log(err);
+            return;
+        }
+        response.json(data);
+    })
+    
+})
+
+app.post('/api', (request, response) => {
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    response.json(data);
+});
